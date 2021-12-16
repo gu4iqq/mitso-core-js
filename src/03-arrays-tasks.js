6c61b6b7c991f48e83e1e063e0b4be152f428299
gu4iqq <vlad-guchok@mail.ru>
2021-12-17 00:14:35 +0300

    3.16

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -279,9 +279,10 @@
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
-}
+ function propagateItemsByPositionIndex(arr) 
+ {
+   return arr.map((x, i) => (new Array(i + 1).fill(x))).flat();
+ }
 
 /**
  * Returns the 3 largest numbers from the specified array
