220bbf5b130ddb1926ebb41b67219a10c9d23bad
gu4iqq <vlad-guchok@mail.ru>
2021-12-17 00:43:34 +0300

    8.3

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -70,9 +70,14 @@
  *   5,10  =>  45 ( = 5+6+7+8+9+10 )
  *   -1,1  =>  0  ( = -1 + 0 + 1 )
  */
-function getSumBetweenNumbers(/* n1, n2 */) {
-  throw new Error('Not implemented');
-}
+ function getSumBetweenNumbers(n1, n2) 
+ {
+   let res = 0;
+   for (let i = n1; i <= n2; i += 1) {
+     res += i;
+   }
+   return res;
+ }
 
 /**
  * Returns true, if a triangle can be built with the specified sides a, b, c
