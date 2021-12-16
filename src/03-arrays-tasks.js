5eaede11d75ac9ac4c897f4f722035783f6fbf4c
gu4iqq <vlad-guchok@mail.ru>
2021-12-17 00:11:16 +0300

    3.10

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -164,9 +164,10 @@
  *    [ 1, 3, 4, 5 ], 2 => [ 1, 3 ]
  *    [ 'a', 'b', 'c', 'd'], 3  => [ 'a', 'b', 'c' ]
  */
-function getHead(/* arr, n */) {
-  throw new Error('Not implemented');
-}
+ function getHead(arr, n) 
+ {
+   return arr.splice(0, n);
+ }
 
 /**
  * Returns the n last items of the specified array
