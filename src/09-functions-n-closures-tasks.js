d1a2afd431b72d76400ed241d6c59d5fd10a68c4
gu4iqq <vlad-guchok@mail.ru>
2021-12-17 00:51:32 +0300

    9.6

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -137,8 +137,14 @@
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+ function logger(func, logFunc) {
+  return (...arg) => {
+    const val = `${func.name}(${JSON.stringify(arg).slice(1, -1)})`;
+    logFunc(`${val} starts`);
+    const res = func(...arg);
+    logFunc(`${val} ends`);
+    return res;
+  };
 }
 
 /**
