54d36aaf471aace1ef1d28619b292f3ab6cf7acb
gu4iqq <vlad-guchok@mail.ru>
2021-12-17 00:51:11 +0300

    9.5

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -97,8 +97,21 @@
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+ function retry(func, attempts) {
+  return () => {
+    for (let i = 0; i <= attempts; i++)
+    {
+      try
+      {
+        return func();
+      } 
+      catch (e) 
+      {
+
+      }
+    }
+    return null;
+  };
 }
 
 /**
