3df5e8ea7ed794a664e1b09ada846c81e83f335d
gu4iqq <vlad-guchok@mail.ru>
2021-12-17 00:43:14 +0300

    8.2

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -52,9 +52,11 @@
  *   5  => 120
  *   10 => 3628800
  */
-function getFactorial(/* n */) {
-  throw new Error('Not implemented');
-}
+ function getFactorial(n) 
+ {
+   if (n === 1) return 1;
+   return n * getFactorial(n - 1);
+ }
 
 /**
  * Returns the sum of integer numbers between n1 and n2 (inclusive).
