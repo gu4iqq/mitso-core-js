3ebf5ecb9a67ff414e48e942584490536132da4a
gu4iqq <vlad-guchok@mail.ru>
2021-12-17 00:13:24 +0300

    3.14

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -239,8 +239,14 @@
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+ function getMovingSum(arr) {
+  let previousSum = 0;
+  return arr.map((item) => {
+    let nick = previousSum;
+    nick += item;
+    previousSum = nick;
+    return nick;
+  });
 }
 
 /**
