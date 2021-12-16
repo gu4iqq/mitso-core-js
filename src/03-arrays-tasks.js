d2c7bc95c69bf9cc2d60e885bc84b19a55497c5a
gu4iqq <vlad-guchok@mail.ru>
2021-12-17 00:07:04 +0300

    3.1

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -19,9 +19,10 @@
  *    ['Array', 'Number', 'string'], 'Date'    => -1
  *    [0, 1, 2, 3, 4, 5], 5    => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
-}
+ function findElement(arr, value) 
+ {
+   return arr.indexOf(value);
+ }
 
 /**
  * Generates an array of odd numbers of the specified length
