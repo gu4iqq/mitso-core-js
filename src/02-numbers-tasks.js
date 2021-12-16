c0ce848f2bfbc7f1d48d777a95d4856360fb6826
gu4iqq <vlad-guchok@mail.ru>
2021-12-16 23:54:32 +0300

    2.10

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -178,8 +178,9 @@
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+ function roundToPowerOfTen(num, pow) {
+  const tenPow = 10 ** pow;
+  return Math.round(num / tenPow) * tenPow;
 }
 
 /**
