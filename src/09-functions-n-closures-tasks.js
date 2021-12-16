f6bc0d8a85c06b1bed0b2d388a13822069507cfa
gu4iqq <vlad-guchok@mail.ru>
2021-12-17 00:50:54 +0300

    9.4

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -77,8 +77,9 @@
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+ function memoize(func) {
+  const result = func();
+  return () => result;
 }
 
 /**
