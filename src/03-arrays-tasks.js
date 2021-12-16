8e3966bbffea52e7a694a590d811de05e3963515
gu4iqq <vlad-guchok@mail.ru>
2021-12-17 00:23:32 +0300

    3.19

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -333,9 +333,23 @@
  *   [ 'nine','eight','nine','eight'] => [ 'eight','eight','nine','nine']
  *   [ 'one','one','one','zero' ]     => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
-}
+ function sortDigitNamesByNumericOrder(arr) 
+ {
+   const values = 
+   {
+     zero: 0, 
+     one: 1,
+     two: 2,
+     three: 3,
+     four: 4,
+     five: 5,
+     six: 6,
+     seven: 7,
+     eight: 8,
+     nine: 9,
+   };
+   return arr.sort((a, b) => values[a] - values[b]);
+ }
 
 /**
  * Returns the sum of all items in the specified array of numbers
